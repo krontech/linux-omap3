294825ea7e99a5dc9fb75422c41af52e06484ee5
Brian Norris <computersforpeace@gmail.com>
2012-02-21 10:38:42 -0800

    ahci: add AHCI_HFLAG_DELAY_ENGINE host flag
    The following commit was intended to fix problems with specific AHCI
    controller(s) that would become bricks if the AHCI specification was not
    followed strictly (that is, if ahci_start_engine() was called while the
    controller was in the wrong state):
    
        commit 7faa33da9b7add01db9f1ad92c6a5d9145e940a7
        ahci: start engine only during soft/hard resets
    
    However, some devices currently have issues with that fix, so we must
    implement a flag that delays the ahci_start_engine() call only for specific
    controllers.
    
    This commit simply introduces the flag, without enabling it in any driver.
    
    Note that even when AHCI_HFLAG_DELAY_ENGINE is not enabled, this patch does
    not constitue a full revert to commit 7faa33da; there is still a change in
    behavior to the ahci_port_suspend() failure path.
    
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>
    Cc: stable@kernel.org
    Signed-off-by: Sriramakrishnan A G <srk@ti.com>

--- drivers/ata/libahci.c
+++ drivers/ata/libahci.c
@@ -717,6 +717,7 @@ static void ahci_power_down(struct ata_port *ap)
 
 static void ahci_start_port(struct ata_port *ap)
 {
+	struct ahci_host_priv *hpriv = ap->host->private_data;
 	struct ahci_port_priv *pp = ap->private_data;
 	struct ata_link *link;
 	struct ahci_em_priv *emp;
@@ -728,6 +729,10 @@ static void ahci_start_port(struct ata_port *ap)
 
 	/* enable DMA */
 	ahci_start_engine(ap);
+
+	/* enable DMA */
+	if (!(hpriv->flags & AHCI_HFLAG_DELAY_ENGINE))
+		ahci_start_engine(ap);
 
 	/* turn on LEDs */
 	if (ap->flags & ATA_FLAG_EM) {
